65c32597c5cba9894e431d201ce1f0cfa80e705f
Geovane Correia <geovanecssantos@outlook.com>
2025-12-18 20:53:19 -0300

    alterando texto

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>alterei</h1>
+      <h1>alterei 3</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
