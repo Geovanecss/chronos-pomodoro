42a95c4ebe23043143a28164762d01869db0d52f
Geovane Correia <geovanecssantos@outlook.com>
2025-12-25 18:04:09 -0300

    Alterando CSS e começando a estruturar a página

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { Heading } from './components/Heading';
-
 import './styles/themes.css';
 import './styles/global.css';
 
 export function App() {
-  console.log('Oi');
-
   return (
     <>
-      <Heading attr={123} attr2='String'>
-        Olá, Mundo!
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum ut
-        commodi eum, fugiat voluptas doloremque quo delectus. Ad saepe molestias
-        quaerat nesciunt debitis dolorum perferendis animi sit? Dolore, fuga
-        ipsa.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
