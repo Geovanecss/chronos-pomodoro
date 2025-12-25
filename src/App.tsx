49bf082965691b245965b41564e222ba045606aa
Geovane Correia <geovanecssantos@outlook.com>
2025-12-25 18:30:04 -0300

    Alterando estrutura de pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import './styles/themes.css';
 import './styles/global.css';
 
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+
 export function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Form</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Footer</Heading>
+      </Container>
     </>
   );
 }
