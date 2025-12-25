a053d130e0e25079341bfd062445ed1abce96199
Geovane Correia <geovanecssantos@outlook.com>
2025-12-25 18:48:30 -0300

    mexendo no css da logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import './styles/themes.css';
-import './styles/global.css';
-
 import { Heading } from './components/Heading';
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
+import './styles/themes.css';
+import './styles/global.css';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
 
       <Container>
